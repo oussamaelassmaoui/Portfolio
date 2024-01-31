fc755c0cf2037ed78fbd04f63bbbc433f1110995
tsm.osm <97128470+oussama12as@users.noreply.github.com>
2024-01-31 18:45:26 +0100

    file seve

---
+++ src/data/constants.js
@@ -0,0 +1,322 @@
+export const Bio = {
+  name: "OUSSAMA EL ASSMAOUI",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/oussamaelassmaoui",
+  resume:
+    "https://drive.google.com/file/d/1JPqmGBvqfrOK8LZjxkQInE9eWYnlvMk6/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/oussama-el-assmaoui-143554251/",
+  twitter: "https://twitter.com/OElassmaoui",
+  insta: "https://www.instagram.com/oussama___elassmaoui?igsh=MWgzZnZ4OWRxeGRuYQ== ",
+  facebook: "https://www.facebook.com/oussama.leassmaui?mibextid=hIlR13",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/tailwind-css-2.svg",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Laravel",
+        image: "https://www.logo.wine/a/logo/Laravel/Laravel-Logo.wine.svg",
+      },
+      {
+        name: "php",
+        image:
+          "https://clipart-library.com/image_gallery2/PHP-Logo.png",
+      },
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+     
+    ],
+  },
+  {
+    title: "CMS",
+    skills: [
+      {
+        name: "Wordpress",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/98/WordPress_blue_logo.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://imgs.search.brave.com/C3qNaKg7MorF0LOw4fiHbAk0gJTUiRpBnWoL55IS5jA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9naXQtaWNvbi5z/dmc.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://imgs.search.brave.com/l3tx5UT3FZ6v5QsmZRdScjF_QwmN_P-PrldEAxIQXhk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9yYXcu/Z2l0aHVidXNlcmNv/bnRlbnQuY29tL2Jh/ZGdlbi9iYWRnZW4t/aWNvbnMvbWFzdGVy/L2ljb25zL2dpdGh1/Yi5zdmc.svg",
+      },
+      {
+        name: "Agile",
+        image:
+          "https://imgs.search.brave.com/KCLOG64lfFLvFewKUZ-JST98injvGOgrwj3r2ckElOc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZnJlZWJpZXN1cHBs/eS5jb20vbG9nb3Mv/bGFyZ2UvMngvYWdp/bGUtc29mdHdhcmUt/MDEtbG9nby1ibGFj/ay1hbmQtd2hpdGUu/cG5n",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://imgs.search.brave.com/50JDDd4L1QxFtnwMrj6rn158HP4VG0qHPxVKtKvCPn4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9wb3N0bWFuLnN2/Zw.svg",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://imgs.search.brave.com/29GQP3JWD8dJXdokqHqKoHX2FZpFWHxPbUyxwzqP5RY/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8w/LzA4L0NhbnZhX2lj/b25fMjAyMS5zdmc.svg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://imgs.search.brave.com/vW62EhmdMfk7HjQovK_01sgTw5y8V14RQQYtRb1jONQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8z/LzMzL0ZpZ21hLWxv/Z28uc3Zn.svg",
+      },
+      {
+        name: "UML",
+        image:
+          "https://i.postimg.cc/sgDcbM1h/wx.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  
+  {
+    id: 0,
+    img: "https://imgs.search.brave.com/ZvV2xAVllKCTZo43pUzDZmk4hv0yPgs21VnlGbGZNWs/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2hhcmlrYS5tYS9y/ZXNzb3VyY2VzL2xv/Z28vMjg0ODg1Lmpw/Zw",
+    role: "Full Stack Developer",
+    company: "WASSLA DIGITAL ",
+    date: "13 Mars 2023 - 22 Avril 2023",
+    desc: "I developed an E-commerce website using ReactJS, Laravel, Bootstrap, JavaScript, UML, Figma .",
+    skills: [
+      "ReactJS",
+      "Laravel",
+      "Bootstrap",
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "UML",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/kinetic-axle-409521.appspot.com/o/CamScanner%2001-02-2024%2017.49_1.jpg?alt=media&token=7877f77b-3abe-4b06-be01-c4fffad8d7ec",
+  },
+  
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://imgs.search.brave.com/RIKK1ighWP30_td9sLI6lFMscCkK4UsM7DW5l-ouHBU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tb25l/bXBsb2kubWEvd3At/Y29udGVudC91cGxv/YWRzLzIwMjAvMDEv/T0ZQUFQtMDEuc3Zn.svg",
+    school: "NTIC ISTA HAY RIAD RABAT",
+    date: "2021-2023",
+    desc: "I completed my education in ntic ista hay riad rabat , where I studied Technicien spécialisé en développement digital option web full Stack.",
+    degree: " DIPLÔME TECHNICIEN SPÉCIALISÉ , Technicien spécialisé en développement digital option web full Stack",
+  },
+  {
+    id: 2,
+    img: "https://www.taallim.com/wp-content/uploads/2021/10/IMG-20211024-WA0028-300x241.jpg",
+    school: "Lycée omar al khayam Taza",
+    date: "2019-2020",
+    degree: "BACCALAURÉAT , science physique et chimie ",
+  },
+ 
+  
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "MOVIE TIME",
+    date: " janvier 2021",
+    description:
+      "MOVIE TIME is  a web application for a website for movies, series, and anime",
+    image:
+      "https://i.postimg.cc/25BbCws6/Capture-d-cran-2024-01-19-232007.jpg",
+    tags: [
+      "html",
+      "css",
+      "JavaScript",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "https://github.com/oussamaelassmaoui/MovieTime.git",
+    webapp: "https://osm-tsm.000webhostapp.com/indix.html",
+  },
+  {
+    id: 2,
+    title: "TSM.SUMMARY",
+    date: " Avril 2023",
+    description:
+      "TSM.SUMMARY is  a web application for Summarizing the articles and simplifying them in order to read them without suffering and reading the most important matters ",
+    image:
+      "https://i.postimg.cc/fknGGwQ5/Capture-d-cran-2024-01-19-232007.jpg",
+    tags: [
+      "html",
+      "css",
+      "JavaScript",
+      "Tailwind CSS",
+      "React Js",
+      "Netlify",
+      "Express Js",
+    ],
+    category: "web app",
+    github: "https://github.com/oussamaelassmaoui/TSM.SUMMARY.git",
+    webapp: "https://clinquant-squirrel-0df1e9.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Chat Application",
+    date: " janvier 2023",
+    description:
+      "Chat Application is  a web application for Chat between friends, and it is possible to send messages and pictures and create chat groups",
+    image:
+      "https://i.postimg.cc/KvH7GjX2/chat-application-main.jpg",
+    tags: [
+      "html",
+      "css",
+      "JavaScript",
+      "Bootstrap",
+      "React Js",
+      "Netlify",
+    ],
+    category: "web app",
+    github: "https://github.com/oussamaelassmaoui/chat_application-main.git",
+    webapp: "https://github.com/oussamaelassmaoui/chat_application-main.git",
+  },
+  {
+    id: 4,
+    title: "HappyShop",
+    date: " 13 Mars 2023 - 22 Avril 2023 ",
+    description:
+      "HappyShop is  a web application for  Selling products is an electronic market in order to facilitate the purchasing process for customers and reduce shopping times, and it supports many actions such as online payment...",
+    image:
+      "https://i.postimg.cc/vmBtbnTb/vc.jpg",
+    tags: [
+      "React Js",
+      "laravel",
+      "html",
+      "css",
+      "JavaScript",
+      "Bootstrap",
+      "API",
+      "SQL",
+    ],
+    category: "web app",
+    github: "https://github.com/oussamaelassmaoui/HappyShopp_ofi.git",
+    webapp: "https://github.com/oussamaelassmaoui/HappyShopp_ofi.git",
+  },
+  {
+    id: 5,
+    title: "Ophtalmologie",
+    date: " Octobre 2023",
+    description:
+      "Ophtalmologie is  a web application for A hospital specializing in ophthalmology. The purpose of the site is to book an appointment and provide articles on eye diseases and their types...",
+    image:
+      "https://i.postimg.cc/1X59jyMC/opt.jpg",
+    tags: [
+      "laravel",
+      "html",
+      "css",
+      "JavaScript",
+      "Bootstrap",
+      "SQL",
+    ],
+    category: "web app",
+    github: "https://github.com/oussamaelassmaoui/Ophtalmologie.git",
+    webapp: "https://ophtalmologie.serv00.net/",
+  },
+  {
+    id: 6,
+    title: "Portfolio",
+    date: " janvier 2024",
+    description:
+      "Portfolio is  a web application for Profile offer skill, projects, experiences, education and communication ... ",
+    image:
+      "https://i.postimg.cc/Gt5HPBrJ/plat.jpg",
+    tags: [
+      "html",
+      "css",
+      "JavaScript",
+      "Bootstrap",
+      "React Js",
+      "Netlify",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021, text: "Started my journey" },
+  { year: 2022, text: "Founded JavaScript Mastery and Laravel" },
+  { year: 2023, text: "Shared my projects with the world" },
+  { year: 2024, text: "Started my own platform" },
+];
